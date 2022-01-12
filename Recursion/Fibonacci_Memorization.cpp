4aeea16eae274311da51da7ccba59cf03c94d5a7
Shamima-Rayhana <85000140+Shamima-Rayhana@users.noreply.github.com>
2022-01-12 13:05:59 +0600

    Fibonacci Number using Memorization

---
+++ Recursion/Fibonacci_Memorization.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int Fibonacci_Array[100];
+
+int fibonacci_using_Memorization(int n)
+{
+    if(n<=1)
+    {
+        Fibonacci_Array[n]=n;
+        return n;
+    }
+    else
+    {
+        if(Fibonacci_Array[n-2]==-1)
+        {
+            Fibonacci_Array[n-2]=fibonacci_using_Memorization(n-2);
+        }
+        if(Fibonacci_Array[n-1]==-1)
+        {
+            Fibonacci_Array[n-1]=fibonacci_using_Memorization(n-1);
+        }
+        Fibonacci_Array[n]=Fibonacci_Array[n-2]+Fibonacci_Array[n-1];
+        return Fibonacci_Array[n-2]+Fibonacci_Array[n-1];
+    }
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the Number"<<endl;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        Fibonacci_Array[i]=-1;
+    }
+    int F = fibonacci_using_Memorization(n);
+    cout<<F<<endl;
+}
+
+
