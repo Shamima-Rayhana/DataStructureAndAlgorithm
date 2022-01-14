a72de41a5fa9094501bc84d4986355dd1d2506a0
Shamima-Rayhana <85000140+Shamima-Rayhana@users.noreply.github.com>
2022-01-14 22:44:55 +0600

    Indirect Recursion

---
+++ Recursion/Indirect Recurssion.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int funB(int n);
+
+int funA(int n)
+{
+    if(n>0)
+    {
+        cout<<n<<" ";
+        funB(n-1);
+    }
+}
+int funB(int n)
+{
+    if(n>1)
+    {
+        cout<<n<<" ";
+        funA(n/2);
+    }
+}
+
+int main()
+{
+    int p;
+    cin>>p;
+    funA(p);
+    return 0;
+}
+
