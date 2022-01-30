1c3eb557f009c8a59a364afe8fba33657e3f3078
Shamima-Rayhana <85000140+Shamima-Rayhana@users.noreply.github.com>
2022-01-30 18:25:18 +0600

    Add / Append Element in an Array

---
+++ Array ADT/AddOrAppendElement.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct array
+{
+    int A[20];
+    int size;
+    int length;
+};
+
+void Display(struct array arr)
+{
+    int i;
+    printf("Array size = %d ",arr.size);
+    printf("\n");
+    printf("Array length = %d ", arr.length);
+    printf("\n");
+    printf("Array elements are \n");
+    for(i=0;i<arr.length;i++)
+    {
+        printf("%d ", arr.A[i]);
+    }
+    printf("\n\n\n");
+}
+
+int main()
+{
+    struct array arr={{5,10,15,20,25},20,5};
+
+    Display(arr);
+    arr.A[arr.length]=30;
+    arr.length++;
+    Display(arr);
+
+
+    return 0;
+}
+
