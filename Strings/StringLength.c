7e2054635291ca7850970c2df919067429c2afaa
Shamima-Rayhana <85000140+Shamima-Rayhana@users.noreply.github.com>
2022-03-31 01:14:08 +0600

    Length of string

---
+++ Strings/StringLength.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+/* Shamima Rayhana Rumi
+   Date:31-03-2022
+   Language: C
+   Content: Length of string
+*/
+
+int main()
+{
+    char *s="Shamima";
+    int i;
+    for(i=0;s[i]!='\0';i++)
+    {
+
+    }
+    printf("Length of the string is : %d\n",i);
+
+    return 0;
+}
+
+
