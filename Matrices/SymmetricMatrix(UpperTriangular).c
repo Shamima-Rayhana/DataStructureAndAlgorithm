c153f109cea8f3f023f94b3a4ecaa0969b7129a4
Shamima-Rayhana <85000140+Shamima-Rayhana@users.noreply.github.com>
2022-05-22 00:10:28 +0600

    Symmetric matrix from upper triangular

---
+++ Matrices/SymmetricMatrix(UpperTriangular).c
@@ -0,0 +1,83 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+/* Shamima Rayhana Rumi
+   Date:21-05-2022
+   Language: C
+   Content: Symmetric matrix (Upper triangular)
+*/
+
+struct matrix{
+    int *A;
+    int n;
+
+};
+
+void set(struct matrix *m, int i,int j,int value)
+{
+    if(i<=j)
+    {
+        m->A[(i-1)*m->n-((i-2)*(i-1))/2+j-i]=value;
+    }
+
+}
+
+int get(struct matrix m, int i,int j)
+{
+    if(i<=j)
+    {
+        return m.A[(i-1)*m.n-((i-2)*(i-1))/2+j-i];
+    }
+    else
+    {
+        return m.A[(j-1)*m.n-((j-2)*(j-1))/2+i-j];
+    }
+
+}
+
+void display(struct matrix m)
+{
+    int i,j;
+    printf("The symmetric matrix is : \n");
+    for(i=1;i<=m.n;i++)
+    {
+        for(j=1;j<=m.n;j++)
+        {
+            if(i<=j)
+                printf("%d ", m.A[(i-1)*m.n-((i-2)*(i-1))/2+j-i]);
+            else
+                printf("%d ", m.A[(j-1)*m.n-((j-2)*(j-1))/2+i-j]);
+        }
+        printf("\n");
+    }
+}
+
+
+
+int main()
+{
+    struct matrix m;
+    int i,j,value;
+    printf("Enter the dimension of the matrix: \n");
+    scanf("%d",&m.n);
+    m.A=(int *)malloc(m.n*(m.n+1)/2*sizeof(int));
+
+    printf("Enter all elements of the matrix: \n");
+    for(i=1;i<=m.n;i++)
+    {
+        for(j=1;j<=m.n;j++)
+        {
+            scanf("%d",&value);
+            set(&m,i,j,value);
+        }
+    }
+    printf("\n");
+
+
+    display(m);
+
+
+    return 0;
+
+}
